36c7cae39d6b83503173320d441fac995a4985d3
TL <manik@Manikanta>
2020-04-11 02:14:26 +0530

    basic structure is created

---
+++ FirstJavaProject/src/com/mnt/Controller/FirstClass.java
@@ -0,0 +1,14 @@
+package com.mnt.Controller;
+
+import java.util.Date;
+
+import javax.xml.crypto.Data;
+
+public class FirstClass 
+{
+    public void getOutput()
+    {
+    	Date date=new Date();
+    	System.out.println(date.getDay());
+    }
+}
