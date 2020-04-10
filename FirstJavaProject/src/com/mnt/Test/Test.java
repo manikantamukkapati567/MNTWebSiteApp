f593f8cc30d626353264f7dba942daafa2bb0214
TL <manik@Manikanta>
2020-04-11 02:23:30 +0530

    Test class is updated

---
+++ FirstJavaProject/src/com/mnt/Test/Test.java
@@ -0,0 +1,12 @@
+package com.mnt.Test;
+
+import com.mnt.Controller.FirstClass;
+
+public class Test 
+{
+   public static void main(String[] args)
+   {
+	  FirstClass fc=new FirstClass();
+	  fc.getOutput();
+}
+}
